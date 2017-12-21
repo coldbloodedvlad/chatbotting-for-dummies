7d9f2698b1fc4b8f2ee538e8240cb4ee26359890
@coldbloodedvlad <vladislavsecondary@gmail.com>
2017-12-21 06:12:14 +0300

    Загрузка, вывод и закрытие основного окна программы.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Влад, ты сделал это!');
+const { app, BrowserWindow } = require('electron');
+
+app.on('ready', () => {
+    let window = new BrowserWindow({
+        show: false
+    });
+
+    window.on('ready-to-show', () => {
+        window.setMenu(null);
+        window.maximize();
+        window.show();
+    });
+
+    window.on('closed', () => window = null);
+
+    window.loadURL('http://vk.com/');
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
